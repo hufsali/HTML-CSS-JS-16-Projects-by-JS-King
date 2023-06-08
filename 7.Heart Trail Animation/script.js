21342636576d5c132e5d49cdbdd978b9b8027928
hufsali <hufsahanif@gmail.com>
2023-06-07 23:44:20 -0700

    added

---
+++ 7.Heart Trail Animation/script.js
@@ -0,0 +1,16 @@
+const bodyEl = document.querySelector("body");
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEL = document.createElement("span");
+  spanEL.style.left = xPos + "px";
+  spanEL.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEL.style.width = size + "px";
+  spanEL.style.height = size + "px";
+
+  bodyEl.appendChild(spanEL);
+  setTimeout(() => {
+    spanEL.remove();
+  }, 3000);
+});
